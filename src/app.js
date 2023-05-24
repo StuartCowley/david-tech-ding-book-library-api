c3c6e3038921f49c4babe55708c5706ae13ddb5c
David Ding <dingtechxing16@gmail.com>
2023-05-24 15:44:21 +0100

    CRUD readers

--- src/app.js
+++ src/app.js
@@ -1,10 +1,18 @@
 const express = require("express")
 const readerController = require("./controllers/reader")
-const { Reader } = require("./models/reader")
+const reader = require("./models/reader")
 const app = express()
 
 app.use(express.json())
 
 app.post("/readers", readerController.createReader)
+
+app.get("/readers", readerController.getAllReaders)
+
+app.get("/readers/:id", readerController.getReaderById)
+
+app.patch("/readers/:id", readerController.updateReader)
+
+app.delete("/readers/:id", readerController.deleteReader)
 
 module.exports = app
--- src/controllers/reader.js
+++ src/controllers/reader.js
@@ -5,4 +5,48 @@ const createReader = async (req, res) => {
   res.status(201).json(newReader)
 }
 
-module.exports = { createReader }
+const getAllReaders = async (_, res) => {
+  const reader = await Reader.findAll()
+  res.status(200).json(reader)
+}
+
+const getReaderById = async (req, res) => {
+  const { id } = req.params
+  const reader = await Reader.findByPk(id)
+
+  if (!reader) {
+    res.status(404).json({ error: "The reader could not be found." })
+  }
+  res.status(200).json(reader)
+}
+
+const updateReader = async (req, res) => {
+  const { id } = req.params
+  const [reader] = await Reader.update(req.body, {
+    where: { id },
+  })
+
+  if (!reader) {
+    res.status(404).json({ error: "The reader could not be found." })
+  }
+
+  res.status(200).json(reader)
+}
+
+const deleteReader = async (req, res) => {
+  const { id } = req.params
+  const [reader] = await Reader.destroy(req.body, {
+    where: { id },
+  })
+  if (!reader) {
+    res.status(404).json({ error: "The reader could not be found." })
+  }
+  res.status(204).json(reader)
+}
+module.exports = {
+  createReader,
+  getAllReaders,
+  getReaderById,
+  updateReader,
+  deleteReader,
+}
